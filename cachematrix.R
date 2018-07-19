90c858c63b170e7ce48f082c8da432636e0e6707
LezakPetr <>
2018-07-19 21:41:55 +0200

    Improved

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implementation of lazy evaluation (e.g. "cache") of the matrix inversion.
 
-## Write a short comment describing this function
-
+## Function makeCacheMatrix creates a "matrix" object, that is actually a list
+## of manipulation functions. These functions manipulates data in their parent
+## environment - the environment of makeCacheMatrix function.
 makeCacheMatrix <- function(x = matrix()) {
-  m <- x;
-  inv <- NULL;
+  m <- x;   # Matrix to be inverted
+  inv <- NULL;   # Inverted matrix
   
   list (
+    # Returns matrix to be inverted
     getMatrix = function() {
-      m
+      return (m);
     },
-    setInverse = function(inverse) {
-      inv <<- inverse;
-    },
-    getInverse = function() {
-      inv
+    # Returns inverted matrix. Does the caching.
+    getInverse = function(...) {
+      if (is.null(inv)) {
+        message ("Calculating inversion");
+        inv <<- solve (m, ...);
+      }
+      
+      return (inv);
     }
   )
 }
 
-
-## Write a short comment describing this function
-
+## Function cacheSolve calculates the inversion of the matrix created by function
+## makeCacheMatrix. It first checks if the inversion has not been calculated
+## and if not it calculates it. Additional parameters are passed to the solve method.
+## The function does not handle change of the additional parameters - if the parameters
+## are different for the first call of the method calculated matrix is stored. The matrix
+## will be returned by next calls of the method even if the additional parameters
+## are changed.
 cacheSolve <- function(x, ...) {
-  if (is.null(x$getInverse())) {
-    message ("Calculating inversion");
-    x$setInverse (solve (x$getMatrix(), ...));
-  }
-  
-  return (x$getInverse());
+  return (x$getInverse(...));
 }
